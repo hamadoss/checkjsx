b6632310f6816df1981eadc60cf11018ced60eb4
hamadoss <hamadaalibi119@gmail.com>
2023-03-20 16:05:55 +0100

    checkjsx

---
+++ src/components/Adresse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adresse = () => {
+  return (
+    <div>
+        <h1>Adresse : hamadaalibi119@gmail.com</h1>
+    </div>
+  )
+}
+
+export default Adresse
---
+++ src/components/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+      <h1>
+        <img src={ProfilePhoto} alt=''/>
+      </h1>
+    </div>
+  )
+}
+
+export default ProfilePhoto
